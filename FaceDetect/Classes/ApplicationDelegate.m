44234125cead24444a2c947ed7245416643223dc
Macmade <macmade@eosgarden.com>
2013-04-28 20:02:10 +0200

    Cleanup...

--- FaceDetect/Classes/ApplicationDelegate.m
+++ FaceDetect/Classes/ApplicationDelegate.m
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, Jean-David Gadina <macmade@eosgarden.com>
+ * Copyright (c) 2011, Jean-David Gadina - www.xs-labs.com
  * Distributed under the Boost Software License, Version 1.0.
  * 
  * Boost Software License - Version 1.0 - August 17th, 2003
@@ -31,7 +31,7 @@
 
 /*!
  * @file        
- * @copyright   eosgarden 2011 - Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright   Jean-David Gadina - www.xs-labs.com
  * @abstract    ...
  */
 
